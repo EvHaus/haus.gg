95296da11968b768952958b6f83e6ab39c0b33e6
Ev Haus <globexdesigns@gmail.com>
2018-04-29 18:43:52 -0700

    HTTPS server works, but certificate still broken

--- server.js
+++ server.js
@@ -9,39 +9,42 @@ const dev = process.env.NODE_ENV !== 'production';
 const app = next({dev});
 const handle = app.getRequestHandler();
 
-if (!dev) {
-	greenlock.create({
-		agreeTos: true,
-		app: express().get('*', (req, res) => {
-			return handle(req, res);
-		}),
-		approveDomains: ['haus.gg', 'www.haus.gg'],
-		configDir: '/tmp/etc/greenlock',
-		email: 'globexdesigns@gmail.com',
-		server: 'https://acme-v02.api.letsencrypt.org/directory',
-		version: 'draft-11',
-	}).listen(80, 443);
-} else {
-	app.prepare()
-		.then(() => {
-			const server = express();
+app.prepare().then(() => {
+	const server = express();
 
-			server.get('*', (req, res) => {
-				return handle(req, res);
-			});
+	server.get('*', (req, res) => {
+		return handle(req, res);
+	});
 
-			// In production `greenlock-express` will take over
-			const port = parseInt(process.env.PORT, 10) || 3000;
+	// In production `greenlock-express` will take over
+	if (!dev) {
+		return greenlock.create({
+			app: server,
+			approveDomains (opts, certs, cb) {
+				opts.domains = certs && certs.altnames || opts.domains;
+				opts.email = 'globexdesigns@gmail.com';
+				opts.agreeTos = true;
 
-			server.listen(port, (err) => {
-				if (err) throw err;
-				// eslint-disable-next-line no-console
-				console.log(`> Ready on http://localhost:${port}`);
-			});
+				cb(null, {
+					certs,
+					options: opts,
+				});
+			},
+			configDir: '/tmp/etc/greenlock',
+			server: 'https://acme-v02.api.letsencrypt.org/directory',
+			version: 'draft-11',
+		}).listen(80, 443);
+	}
 
-			return server;
-		})
-		.catch((err) => {
-			throw err;
-		});
-}
+	const port = parseInt(process.env.PORT, 10) || 3000;
+
+	server.listen(port, (err) => {
+		if (err) throw err;
+		// eslint-disable-next-line no-console
+		console.log(`> Ready on http://localhost:${port}`);
+	});
+
+	return server;
+}).catch((err) => {
+	throw err;
+});
