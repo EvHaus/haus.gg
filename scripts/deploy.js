64631ab4c4ce58e12a3ce59398364c589fc86eb5
Ev Haus <globexdesigns@gmail.com>
2018-06-19 19:51:17 -0700

    Switch to latest deployment file

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -3,6 +3,8 @@
 const Deploy = require('@globexdesigns/deploy');
 
 const D = new Deploy({
+	directories: ['.next', 'static'],
+	files: ['package.json', 'server.js', 'yarn.lock'],
 	host: '138.68.241.10',
 	private_key_path: '/home/globex/.ssh/id_rsa',
 	target_dir: '/var/www',
