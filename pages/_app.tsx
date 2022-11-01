37f2af2c163eac04e259127a65e052e1c5d8a9d2
Ev Haus <ev@haus.gg>
2022-10-31 20:44:37 -0700

    Some clean up and fix for orb animation

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,51 +1,43 @@
 import './_app.css';
 import 'tippy.js/dist/tippy.css';
-import {initGA, logPageView} from '../utils/analytics';
 import React, {useEffect} from 'react';
 import {SITE_AUTHOR, SITE_DESC, SITE_NAME, TWITTER_USER} from '../constants/seo';
 import type {AppProps} from 'next/app';
 import Head from 'next/head';
+import {GoogleAnalytics} from 'nextjs-google-analytics';
 import {Montserrat} from '@next/font/google';
-import Router from 'next/router';
 
 const monserrat = Montserrat({subsets: ['latin']});
 
-const _app = ({ Component, pageProps }: AppProps) => {
-	useEffect(() => {
-		initGA();
-		logPageView();
-		Router.events.on('routeChangeComplete', logPageView);
-	}, []);
-
-	return (
-		<>
-			<Head>
-				<title>{SITE_NAME}</title>
-				<meta charSet='utf-8' />
-				<meta content='width=device-width,initial-scale=1,shrink-to-fit=no' name='viewport' />
-				<meta content={SITE_NAME} name='application-name' />
-				<meta content={SITE_DESC} name='description' />
-				<meta content='next.js' name='generator' />
-				<meta content='General' name='rating' />
-				<meta content='#222' name='theme-color' />
-				<meta content={SITE_AUTHOR} property='article:author' />
-				<meta content={SITE_DESC} property='og:description' />
-				<meta content='en_US' property='og:locale' />
-				<meta content={SITE_NAME} property='og:site_name' />
-				<meta content={SITE_NAME} property='og:title' />
-				<meta content='website' property='og:type' />
-				<meta content='summary' property='twitter:card' />
-				<meta content={TWITTER_USER} property='twitter:creator' />
-				<meta content={SITE_DESC} property='twitter:description' />
-				<meta content={SITE_NAME} property='twitter:title' />
-				<link href='https://www.google-analytics.com' rel='preconnect' />
-			</Head>
-			<div className={monserrat.className}>
-				{/* eslint-disable-next-line react/jsx-props-no-spreading */}
-				<Component {...pageProps} />
-			</div>
-		</>
-	);
-};
+const _app = ({ Component, pageProps }: AppProps) => (
+	<>
+		<Head>
+			<title>{SITE_NAME}</title>
+			<meta charSet='utf-8' />
+			<meta content='width=device-width,initial-scale=1,shrink-to-fit=no' name='viewport' />
+			<meta content={SITE_NAME} name='application-name' />
+			<meta content={SITE_DESC} name='description' />
+			<meta content='next.js' name='generator' />
+			<meta content='General' name='rating' />
+			<meta content='#222' name='theme-color' />
+			<meta content={SITE_AUTHOR} property='article:author' />
+			<meta content={SITE_DESC} property='og:description' />
+			<meta content='en_US' property='og:locale' />
+			<meta content={SITE_NAME} property='og:site_name' />
+			<meta content={SITE_NAME} property='og:title' />
+			<meta content='website' property='og:type' />
+			<meta content='summary' property='twitter:card' />
+			<meta content={TWITTER_USER} property='twitter:creator' />
+			<meta content={SITE_DESC} property='twitter:description' />
+			<meta content={SITE_NAME} property='twitter:title' />
+			<link href='https://www.google-analytics.com' rel='preconnect' />
+		</Head>
+		<div className={monserrat.className}>
+			<GoogleAnalytics />
+			{/* eslint-disable-next-line react/jsx-props-no-spreading */}
+			<Component {...pageProps} />
+		</div>
+	</>
+);
 
 export default _app;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,54 +1,27 @@
-import React, {useLayoutEffect} from 'react';
 import Button from '../components/Button';
+import React from 'react';
 import SocialIcon from '../components/SocialIcon';
 import styles from './index.module.css';
 
-type PropsType = {
-	// Array of colors for the circle
-	circleColors: Array<string>,
-	// Number of circles in animation
-	circleCount: number,
-	// Opacity of circles in animation
-	circleOpacity: number,
-	// Speed of animation in ms
-	circleSpeed: number,
-};
+// Array of colors for the circle
+const CIRCLE_COLORS = [
+	'#E9FCC7',
+	'#FDE0A8',
+	'#FFB99B',
+	'#CC8E9A',
+	'#686F80',
+	'#919FB6',
+	'#4F4F61',
+];
 
-const Index = ({
-	circleColors = [
-		'#E9FCC7',
-		'#FDE0A8',
-		'#FFB99B',
-		'#CC8E9A',
-		'#686F80',
-		'#919FB6',
-		'#4F4F61',
-	],
-	circleCount = 6,
-	circleOpacity = 0.6,
-	circleSpeed = 10000,
-}: PropsType) => {
-	useLayoutEffect(() => {
-		const el = document.getElementsByTagName('html')[0];
-		const c = document.createComment(`              _
-			     | |
-	  _____   __ | |__   __ _ _   _ ___
-	 / _ \\ \\ / / | '_ \\ / _\` | | | / __|
-	|  __/\\ V /  | | | | (_| | |_| \\__ \\
-	 \\___| \\_/   |_| |_|\\__,_|\\__,_|___/
+// Number of circles in animation
+const CIRCLE_COUNT = 6;
 
-	If this code works - I wrote it.
-	Otherwise, I don't know where it came from.`);
-		el.prepend(c);
-	}, []);
+// Opacity of circles in animation
+const CIRCLE_OPACITY = 0.6;
 
-	const orbStyle = {
-		animationDuration: `${circleSpeed}ms`,
-	};
-
-	const circleStyle = {
-		opacity: circleOpacity,
-	};
+const Index = () => {
+	const circleStyle = { opacity: CIRCLE_OPACITY };
 
 	return (
 		<div className={styles.view}>
@@ -56,19 +29,21 @@ const Index = ({
 				<span className={styles.logoEv}>EV</span>
 				<span className={styles.logoHaus}>HAUS</span>
 			</h1>
-			<div className={styles.orb} style={orbStyle}>
-				{new Array(circleCount).map((v, i) => {
-					const circleStyleUnique = {
-						color: circleColors[i],
-					};
+			<div className={styles.orb}>
+				<div className={styles.orbInner}>
+					{Array.from(new Array(CIRCLE_COUNT)).map((v, i) => {
+						const circleStyleUnique = {
+							color: CIRCLE_COLORS[i],
+						};
 
-					return (
-						<div
-							className={styles.orbCircle}
-							key={i}
-							style={{...circleStyle, ...circleStyleUnique}} />
-					);
-				})}
+						return (
+							<div
+								className={styles.orbCircle}
+								key={i}
+								style={{...circleStyle, ...circleStyleUnique}} />
+						);
+					})}
+				</div>
 			</div>
 			<div className={styles.resume}>
 				<Button label='My Resume' link='/resume-ev-haus.pdf' />
