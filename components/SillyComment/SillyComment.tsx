8b42749277cf6a88e04cdf62bbb9ea8d6d1f2745
Ev Haus <ev@haus.gg>
2022-11-12 17:18:22 -0800

    Add comment timeout

--- components/SillyComment/SillyComment.tsx
+++ components/SillyComment/SillyComment.tsx
@@ -8,17 +8,20 @@ let isRendered = false;
 const SillyComment = () => {
 	useEffect(() => {
 		if (isRendered) {
-			const comment = document.createComment(`
-               _
-              | |
-   _____   __ | |__   __ _ _   _ ___
-  / _ \\ \\ / / | '_ \\ / _' | | | / __|
- |  __/\\ V /  | | | | (_| | |_| \\__ \\
-  \\___| \\_/   |_| |_|\\__,_|\\__,_|___/
+			// Timeout is needed otherwise Vercel strips the comments on prod
+			setTimeout(() => {
+				const comment = document.createComment(`
+				_
+				| |
+	_____   __ | |__   __ _ _   _ ___
+	/ _ \\ \\ / / | '_ \\ / _' | | | / __|
+	|  __/\\ V /  | | | | (_| | |_| \\__ \\
+	\\___| \\_/   |_| |_|\\__,_|\\__,_|___/
 
- If this code works - I wrote it.
- Otherwise, I don't know where it came from. `);
-			document.documentElement.insertBefore(comment, document.documentElement.firstChild);
+	If this code works - I wrote it.
+	Otherwise, I don't know where it came from. `);
+				document.documentElement.insertBefore(comment, document.documentElement.firstChild);
+			}, 1000);
 		}
 		isRendered = true;
 	}, []);
