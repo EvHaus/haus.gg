4a12309cd763199bd6691949e305ae1ed8b9f275
Ev Haus <ev@haus.gg>
2022-11-12 17:02:51 -0800

    Remove unnecessary characters

--- components/SillyComment/SillyComment.tsx
+++ components/SillyComment/SillyComment.tsx
@@ -9,16 +9,15 @@ const SillyComment = () => {
 	useEffect(() => {
 		if (isRendered) {
 			const comment = document.createComment(`
-/*               _
- *              | |
- *   _____   __ | |__   __ _ _   _ ___
- *  / _ \\ \\ / / | '_ \\ / _' | | | / __|
- * |  __/\\ V /  | | | | (_| | |_| \\__ \\
- *  \\___| \\_/   |_| |_|\\__,_|\\__,_|___/
- *
- * If this code works - I wrote it.
- * Otherwise, I don't know where it came from.
- */`);
+               _
+              | |
+   _____   __ | |__   __ _ _   _ ___
+  / _ \\ \\ / / | '_ \\ / _' | | | / __|
+ |  __/\\ V /  | | | | (_| | |_| \\__ \\
+  \\___| \\_/   |_| |_|\\__,_|\\__,_|___/
+
+ If this code works - I wrote it.
+ Otherwise, I don't know where it came from. `);
 			document.documentElement.insertBefore(comment, document.documentElement.firstChild);
 		}
 		isRendered = true;
