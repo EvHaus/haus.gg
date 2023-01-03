ae4ee1d7c233f512856d137e2ea0e18e410fbf7a
Ev Haus <ev@haus.gg>
2023-01-02 16:29:48 -0800

    Try Vercel Analytics

--- components/Analytics/Analytics.tsx
+++ components/Analytics/Analytics.tsx
@@ -2,9 +2,14 @@
 
 import {GoogleAnalytics} from 'nextjs-google-analytics';
 import React from 'react';
+// eslint-disable-next-line import/no-unresolved
+import {Analytics as VercelAnalytics} from '@vercel/analytics/react';
 
 const Analytics = () => (
-	<GoogleAnalytics trackPageViews={true} />
+	<>
+		<GoogleAnalytics trackPageViews={true} />
+		<VercelAnalytics />
+	</>
 );
 
 export default Analytics;
